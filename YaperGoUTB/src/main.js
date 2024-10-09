8081862f513ecf4fd7eeaff95ba4aa2fd3fbfdd1
Wollman <159662270+Wmadiedo@users.noreply.github.com>
2024-10-08 21:21:27 -0500

    Delete YaperGoUTB directory

--- YaperGoUTB/src/main.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import VCalendar from 'v-calendar'; // Importar VCalendar
-
-const app = createApp(App);
-
-// Use VCalendar globally
-app.use(VCalendar, { componentPrefix: 'vc' }); // Puedes personalizar el prefijo si quieres
-app.mount('#app');
