6ed4dfd434e0b7443d4a16b4f9f9e02c7535ccc0
iCruzDaniel <sp_dicruz044@hotmail.com>
2024-11-02 03:27:41 -0500

    Adición de Bootstrap al proyecto y creación del componente para Filtro

---
+++ src/components/Filtros.vue
@@ -0,0 +1,88 @@
+<script setup>
+import {ref} from 'vue';
+
+//Array de los valores de los Switches
+const filtros_marcados = ref([
+    true, 
+    true, 
+    true, 
+    true, 
+    true
+]); 
+
+</script>
+
+<template>
+
+    <div class="container my-5">
+    <h3>Filtros</h3>
+
+        <div class="form-check form-switch switch-verde">
+            <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckChecked" v-model="filtros_marcados[0]"/>
+            <label class="form-check-label" for="flexSwitchCheckChecked">Documentos Personales</label>
+        </div>
+        <div class="form-check form-switch switch-rojo">
+            <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckChecked" v-model="filtros_marcados[1]"/>
+            <label class="form-check-label" for="flexSwitchCheckChecked">Libros\Cuadernos</label>
+        </div>
+        <div class="form-check form-switch switch-azul">
+            <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckChecked" v-model="filtros_marcados[2]"/>
+            <label class="form-check-label" for="flexSwitchCheckChecked">Termos\Loncheras</label>
+        </div>
+        <div class="form-check form-switch switch-amarillo">
+            <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckChecked" v-model="filtros_marcados[3]"/>
+            <label class="form-check-label" for="flexSwitchCheckChecked">Termos\Cartesras\</label>
+        </div>
+        <div class="form-check form-switch switch-purpura">
+            <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckChecked" v-model="filtros_marcados[4]"/>
+            <label class="form-check-label" for="flexSwitchCheckChecked">Ropa\Calzado\Sombreros</label>
+        </div>
+    </div>
+
+</template>
+
+<style scoped>
+/* Base para los switches */
+.form-check-input {
+  background-color: #ccc;
+  border-color: #ccc;
+}
+
+/* Estilo general para switches activados */
+.form-check-input:checked {
+  border-color: inherit;
+  background-color: inherit;
+}
+
+/* Switch verde */
+.switch-verde .form-check-input:checked {
+  background-color: #4CAF50;
+  border-color: #4CAF50;
+}
+
+/* Switch azul */
+.switch-azul .form-check-input:checked {
+  background-color: #2EA1DA;
+  border-color: #2EA1DA;
+}
+
+/* Switch rojo */
+.switch-rojo .form-check-input:checked {
+  background-color: #E83879;
+  border-color: #E83879;
+}
+
+/* Switch amarillo */
+.switch-amarillo .form-check-input:checked {
+  background-color: #C2DA2E;
+  border-color: #C2DA2E;
+}
+
+/* Switch amarillo */
+.switch-purpura .form-check-input:checked {
+  background-color: #A32EDA;
+  border-color: #A32EDA;
+}
+
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,23 @@
-import './assets/main.css'
+//import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+
+//import Vue from 'vue'
+//import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+
+// Import Bootstrap and BootstrapVue CSS files (order is important)
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+//import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+// Make BootstrapVue available throughout your project
+//Vue.use(BootstrapVue)
+// Optionally install the BootstrapVue icon components plugin
+//Vue.use(IconsPlugin)
 
 const app = createApp(App)
 
